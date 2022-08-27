93f0c6bc4a7253cb9dcaf10141869b76927e27d6
Hiago Prates <hiagohps360@gmail.com>
2022-08-27 15:51:24 -0300

    feat: button signin component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { FC } from 'react';
+import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss'
 
@@ -8,10 +10,13 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
 
+
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   )
