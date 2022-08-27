1cbc16719a93d130cc978f003cb89a9ac028f59b
Hiago Prates <hiagohps360@gmail.com>
2022-08-27 14:49:52 -0300

    feat: fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/home.module.scss'
 export default function Home() {
   return (
     <h1 className={styles.title}>
-      Hello world ignews
+      Hello world
     </h1>
   )
 }
